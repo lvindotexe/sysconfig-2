93edf3fa7ef98a1f39620743441a66076047320a
alvin <al@pop-os.localdomain>
2024-04-01 21:25:58 +0200

    made script sh insted of bash

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -9,9 +9,6 @@ SSH_DIR="$HOME/.ssh"
 VALUES_FILE="$HOME/.config/dotfiles/values.yml"
 REPO_URL="https://github.com/lvindotexe/sysconfig-2.git"
 
-# The extra_args variable will be set based on the current non-root user
-extra_args=""
-
 # Check if the script is running as root and warn the user
 if [ "$(id -u)" -eq 0 ]; then
     echo "Warning: It's not recommended to run this script as root."
@@ -19,17 +16,18 @@ if [ "$(id -u)" -eq 0 ]; then
 fi
 
 # Check for the existence of values.yml in the current directory
-
 if [ ! -f "$VALUES_FILE" ]; then
   echo "The file $VALUES_FILE does not exist. Please generate it."
   
-  read -p "Would you like to generate $VALUES_FILE now? (y/n) " answer
+  printf "Would you like to generate %s now? (y/n) " "$VALUES_FILE"
+  read answer
   if [ "$answer" = "y" ]; then
     echo "Generating $VALUES_FILE..."
     mkdir -p "$HOME/.config/dotfiles"
-    # Ask the user for their Git username and email
-    read -p "Enter your Git user name: " git_user_name
-    read -p "Enter your Git user email: " git_user_email
+    printf "Enter your Git user name: "
+    read git_user_name
+    printf "Enter your Git user email: "
+    read git_user_email
 
     # Generate values.yml with the user's input
     echo "---" > "$VALUES_FILE"  # Start of the YAML file
@@ -42,7 +40,6 @@ if [ ! -f "$VALUES_FILE" ]; then
     exit 1
   fi
 fi
-
 
 # Function to install Ansible based on the detected package manager
 install_ansible() {
@@ -67,7 +64,7 @@ install_ansible() {
 update_or_clone_repo() {
     if [ -d "$DOTFILES_DIR" ]; then
         echo "Repository folder exists. Checking for updates..."
-        cd "$DOTFILES_DIR"
+        cd "$DOTFILES_DIR" || exit
         git fetch
         if git status | grep -q "Your branch is behind"; then
             echo "Repository is behind. Pulling changes..."
@@ -78,7 +75,7 @@ update_or_clone_repo() {
     else
         echo "Cloning the repository..."
         git clone "$REPO_URL" "$DOTFILES_DIR" || { echo "Failed to clone repository."; exit 1; }
-        cd "$DOTFILES_DIR"
+        cd "$DOTFILES_DIR" || exit
     fi
 }
 
@@ -88,8 +85,6 @@ update_or_clone_repo
 # Check for Ansible and install if not present
 if ! command -v ansible >/dev/null 2>&1; then
     echo "Ansible is not installed. Attempting to install Ansible..."
-
-    # Detect package manager and install Ansible
     if [ -f "/etc/os-release" ]; then
         . /etc/os-release
         case "$ID" in
@@ -113,14 +108,9 @@ else
 fi
 
 # Generate SSH keys
-# Check if SSH directory exists and has the necessary files
-if [[ -f "$SSH_DIR/id_rsa" ]] && [[ -f "$SSH_DIR/id_rsa.pub" ]]; then
+if [ -f "$SSH_DIR/id_rsa" ] && [ -f "$SSH_DIR/id_rsa.pub" ]; then
   echo "Existing SSH keys found. Using them."
-
-  # Ensure the SSH directory permissions are correct
   chmod 700 "$SSH_DIR"
-
-  # Append the public key to authorized_keys if it's not already there
   if ! grep -qF "$(cat "$SSH_DIR/id_rsa.pub")" "$SSH_DIR/authorized_keys" 2>/dev/null; then
     cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
     echo "Public key added to authorized_keys."
@@ -128,32 +118,24 @@ if [[ -f "$SSH_DIR/id_rsa" ]] && [[ -f "$SSH_DIR/id_rsa.pub" ]]; then
     echo "Public key already in authorized_keys."
   fi
 else
-  # If the keys don't exist, create the directory, generate the keys, and add to authorized_keys
   echo "No existing SSH keys found. Generating new keys."
-  
   mkdir -p "$SSH_DIR"
   chmod 700 "$SSH_DIR"
-
   ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
-  
   cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
   echo "New SSH keys generated and added to authorized_keys."
 fi
 
-# Ensure authorized_keys file permissions are correct
-if [[ -f "$SSH_DIR/authorized_keys" ]]; then
+if [ -f "$SSH_DIR/authorized_keys" ]; then
   chmod 600 "$SSH_DIR/authorized_keys"
 fi
 
 echo "SSH key setup complete."
 
-
-# Update Galaxy
 ansible-galaxy install -r requirements.yml
 
-# Run playbook
 echo "Running the Ansible playbook..."
-if [[ -f "$CONFIG_DIR/vault-password.txt" ]]; then
+if [ -f "$CONFIG_DIR/vault-password.txt" ]; then
   ansible-playbook --diff --extra-vars "@$CONFIG_DIR/values.yml" --vault-password-file "$CONFIG_DIR/vault-password.txt" "$DOTFILES_DIR/main.yml" "$@"
 else
   ansible-playbook --diff --extra-vars "@$CONFIG_DIR/values.yml" "$DOTFILES_DIR/main.yml" "$@" --ask-become-pass 
